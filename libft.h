645c9ccd7d06f0a8bcc7855c0f4b759ca2ff4101
Peter Harbst <pharbst@2-E-6.42heilbronn.de>
2022-05-20 18:39:02 +0200

    modifed: added some function declarations

--- libft.h
+++ libft.h
@@ -68,4 +68,10 @@ void		ft_lstclear(t_list **lst, void (*del)(void*));
 void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
+char	*ft_itohex(unsigned long i);
+char	*ft_strjoinfree(char const *str1, char const *str2);
+char	*ft_strlower(char *str);
+char	*ft_strupper(char *str);
+char	*ft_uitoa(unsigned int n);
+
 #endif
