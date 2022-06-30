6a4adec189a167aee6e672ff06594990b00d8104
Peter Harbst <pharbst@student.42heilbronn.de>
2022-06-30 18:28:38 +0200

    finished bufferfunctions

--- src/additional/ft_bufferjoin.c
+++ src/additional/ft_bufferjoin.c
@@ -6,28 +6,29 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 19:41:37 by pharbst           #+#    #+#             */
-/*   Updated: 2022/06/13 20:51:24 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/06/30 18:26:37 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft.h"
 
-int	ft_bufferjoin(char **buff, char *jstr, int buff_len, int jlen)
+t_buff	*ft_bufferjoin(t_buff *buff)
 {
-	char	*new_buff;
-
-	if (!jstr)
-		return (buff_len);
-	if (jlen == 0)
-		jlen = ft_strlen(jstr);
-	new_buff = malloc((buff_len + jlen) * sizeof(char));
-	if (!new_buff)
-		return (buff_len);
-	ft_memcpy(new_buff, *buff, buff_len);
-	ft_memcpy(new_buff + buff_len, jstr, jlen);
-	if (*buff)
-		free(*buff);
-	free(jstr);
-	*buff = new_buff;
-	return (buff_len + jlen);
+	if ((*buff).jlen == 0)
+		(*buff).jlen = ft_strlen((*buff).jstr);
+	if ((*buff).len + (*buff).jlen > (*buff).maxlen)
+	{
+		(*buff).new_buff = (char *)ft_calloc(((*buff).len
+					+ (*buff).jlen + 100), sizeof(char));
+		if (!(*buff).new_buff)
+			return (NULL);
+		ft_memcpy((*buff).new_buff, (*buff).buffer, (*buff).len);
+		ft_memcpy(((*buff).new_buff + (*buff).len), (*buff).jstr, (*buff).jlen);
+		if ((*buff).buffer)
+			free((*buff).buffer);
+		(*buff).buffer = (*buff).new_buff;
+	}
+	else
+		ft_memcpy((*buff).buffer + (*buff).len, (*buff).jstr, (*buff).jlen);
+	return (buff);
 }
--- src/additional/ft_buffinit.c
+++ src/additional/ft_buffinit.c
@@ -6,19 +6,20 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 17:25:06 by pharbst           #+#    #+#             */
-/*   Updated: 2022/06/30 17:27:27 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/06/30 17:59:44 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft.h"
 
-t_buff	ft_buffinit()
+t_buff	ft_buffinit(void)
 {
 	t_buff	buff;
 
 	buff.maxlen = 0;
-	buff.len	= 0;
-	buff.jlen	= 0;
-	buff.buffer	= NULL;
-	buff.jstr	= NULL;
+	buff.len = 0;
+	buff.jlen = 0;
+	buff.buffer = NULL;
+	buff.new_buff = NULL;
+	buff.jstr = NULL;
 }
