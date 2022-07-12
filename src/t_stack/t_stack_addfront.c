ef03ea236c3942c4199b1b41de26192356201b7e
Peter Harbst <pharbst@student.42heilbronn.de>
2022-07-12 17:13:56 +0200

    deleted printf and stdio from the stackadd front function

--- src/t_stack/t_stack_addfront.c
+++ src/t_stack/t_stack_addfront.c
@@ -6,21 +6,18 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 14:35:05 by pharbst           #+#    #+#             */
-/*   Updated: 2022/07/12 17:02:06 by pharbst          ###   ########.fr       */
+/*   Updated: 2022/07/12 17:10:58 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../libft.h"
-#include <stdio.h>
 
 t_stack	*ft_stackadd_front(t_stack *stack, t_stack *newnode)
 {
-	printf("stackendfront test");
 	t_stack	*last;
 
 	if (stack)
 	{
-		printf("stackaddfront start");
 		newnode->next = stack;
 		last = ft_stacklast(stack);
 		last->next = newnode;
