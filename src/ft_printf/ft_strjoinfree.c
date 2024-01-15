10e1ed020157e91daf38522c356cc2ab4eb0a4d6
upperdim <45048352+upperdim@users.noreply.github.com>
2024-01-15 17:59:09 +0100

    fix leak upon error

--- src/ft_printf/ft_strjoinfree.c
+++ src/ft_printf/ft_strjoinfree.c
@@ -26,7 +26,10 @@ char	*ft_strjoinfree(char *str1, char *str2)
 		*str1 = '\0';
 	}
 	if (!str2)
+	{
+		free(str1);
 		return (str1);
+	}
 	len1 = ft_strlen(str1);
 	len2 = ft_strlen(str2);
 	dest = malloc((len1 + len2 + 1) * sizeof(char));
